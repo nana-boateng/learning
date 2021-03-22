45e4fca693d7da25abf3e915a87f882766ed00d7
Nana Boateng <nana.boateng@queensu.ca>
2021-03-22 18:34:08 -0400

    Add register and authorize routes

--- auth-with-node/src/index.js
+++ auth-with-node/src/index.js
@@ -6,12 +6,17 @@ import "./env.js";
 import { fastify } from "fastify";
 // fastify-static is a plugin that makes hosting a static file directory very easy
 import fastifyStatic from "fastify-static";
-
+//fastify cookie
+import fastifyCookie from "fastify-cookie";
 // while __dirname and __filename are usually a default in node
 // with ESM you have to make them yourself.
 // Feature, not a bug
 import path from "path";
 import { fileURLToPath } from "url";
+
+import { registerUser } from "./accounts/register.js";
+import { authorizeUser } from "./accounts/authorize.js";
+import { signUserIn } from "./accounts/signUserIn.js";
 
 import { connectDb } from "./db.js";
 
@@ -29,15 +34,62 @@ async function startApp() {
   try {
     // registers the static file directory
     // as the current directory { __dirname } with the "public" folder inside of it
+
+    app.register(fastifyCookie, {
+      secret: process.env.COOKIE_SIGNATURE,
+    });
+
     app.register(fastifyStatic, {
       root: path.join(__dirname, "public"),
     });
 
-    // return "Hello again" when you visit localhost:3000/get
-    // app.get("/", {}, () => {})
-    app.get("/get", {}, (request, reply) => {
-      reply.send({ data: "Hello again 👋" });
+    app.post("/api/register", {}, async (request, reply) => {
+      try {
+        const userId = await registerUser(
+          request.body.email,
+          request.body.password
+        );
+      } catch (error) {
+        console.log(error);
+      }
     });
+
+    app.post("/api/authorize", {}, async (request, reply) => {
+      try {
+        const { isAuthorized, userId } = await authorizeUser(
+          request.body.email,
+          request.body.password
+        );
+        if (isAuthorized) {
+          // generate auth token
+          // set cookies
+          await signUserIn(userId, request, reply);
+          reply.send({
+            data: "Success!",
+          });
+        } else {
+          reply.send({
+            data: "auth-fail",
+          });
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    });
+
+    app.get("/test", {}, (request, reply) => {
+      console.log(request.cookies.testCookie);
+      reply.send({
+        data: "👨👨‍👨👨👨🦳",
+      });
+    });
+
+    // return "Hello again" when you visit localhost:3000/say-hi
+    // app.get("/", {}, () => {})
+    app.get("/say-hi", {}, (request, reply) => {
+      reply.send({ data: "Hello 👋" });
+    });
+
     // remember the function returns a promise so use this await
     // to wait for a fulfilment
     console.log("🚀 Server listening from port 3000");
