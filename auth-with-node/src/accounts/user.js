0ed7557d0fa4e405a1c6d1cc81fadf6a77ca3112
Nana Boateng <nana.boateng@queensu.ca>
2021-03-24 13:29:05 -0400

    Scaffold getting user data from cookies

---
+++ auth-with-node/src/accounts/user.js
@@ -0,0 +1,23 @@
+export async function getUserFromCookies(request, reply) {
+  try {
+    // get access and refresh tokens
+    // if access token
+    // decode access token
+    // return user from record
+    // decode refresh token
+    // look up session
+    // if session valid
+    // look up current user
+    // refresh tokens
+    // return current user
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function refreshTokens() {
+  try {
+  } catch (error) {
+    console.error(error);
+  }
+}
